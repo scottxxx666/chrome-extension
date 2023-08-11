8a75455013aaff2259770e0a8d4b807bded0d642
Scott Kao <scottxxx666@gmail.com>
2023-08-12 01:43:50 +0800

    feat: override websocket origin header

--- background.js
+++ background.js
@@ -4,6 +4,32 @@ chrome.runtime.onInstalled.addListener(() => {
     });
 });
 
+function addRuleOverwriteOrigin() {
+    chrome.declarativeNetRequest.updateDynamicRules({
+            addRules: [
+                {
+                    action: {
+                        type: "modifyHeaders",
+                        requestHeaders: [
+                            {
+                                header: "origin",
+                                operation: "set",
+                                value: "https://term.ptt3.cc/"
+                            }
+                        ]
+                    },
+                    condition: {
+                        urlFilter: 'wss://ws.ptt.cc/bbs', //block this websocket
+                        domains: ["term.ptt.cc"], // on this domain
+                    },
+                    id: 2,
+                    priority: 1,
+                },
+            ],
+        },
+        () => console.log("block rule added"));
+}
+
 chrome.action.onClicked.addListener(async (tab) => {
     console.log("cliock")
     // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
@@ -13,21 +39,20 @@ chrome.action.onClicked.addListener(async (tab) => {
 
     // Set the action badge to the next state
     await chrome.action.setBadgeText({
-        tabId: tab.id,
         text: nextState,
     });
 
     if (nextState === "ON") {
-        // Insert the CSS file when the user turns the extension on
-        await chrome.scripting.insertCSS({
-            files: ["focus-mode.css"],
-            target: { tabId: tab.id },
-        });
+        addRuleOverwriteOrigin();
     } else if (nextState === "OFF") {
-        // Remove the CSS file when the user turns the extension off
-        await chrome.scripting.removeCSS({
-            files: ["focus-mode.css"],
-            target: { tabId: tab.id },
-        });
+        await removeAllRules();
     }
 })
+
+async function removeAllRules() {
+    const oldRules = await chrome.declarativeNetRequest.getDynamicRules();
+    chrome.declarativeNetRequest.updateDynamicRules(
+        {removeRuleIds: oldRules.map(e => e.id)},
+        () => console.log("block rule added");
+    );
+}
