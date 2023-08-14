d800fcab2d61f9a9192ed41a3400bca551441abf
Scott Kao <scottxxx666@gmail.com>
2023-08-14 23:49:02 +0800

    fix: remove not working modify websocket header

--- background.js
+++ background.js
@@ -4,34 +4,7 @@ chrome.runtime.onInstalled.addListener(() => {
     });
 });
 
-function addRuleOverwriteOrigin() {
-    chrome.declarativeNetRequest.updateDynamicRules({
-            addRules: [
-                {
-                    action: {
-                        type: "modifyHeaders",
-                        requestHeaders: [
-                            {
-                                header: "origin",
-                                operation: "set",
-                                value: "https://term.ptt3.cc/"
-                            }
-                        ]
-                    },
-                    condition: {
-                        urlFilter: 'wss://ws.ptt.cc/bbs', //block this websocket
-                        domains: ["term.ptt.cc"], // on this domain
-                    },
-                    id: 2,
-                    priority: 1,
-                },
-            ],
-        },
-        () => console.log("block rule added"));
-}
-
 chrome.action.onClicked.addListener(async (tab) => {
-    console.log("cliock")
     // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
     const prevState = await chrome.action.getBadgeText({tabId: tab.id});
     // Next state will always be the opposite
@@ -43,16 +16,16 @@ chrome.action.onClicked.addListener(async (tab) => {
     });
 
     if (nextState === "ON") {
-        addRuleOverwriteOrigin();
+        startExtension();
     } else if (nextState === "OFF") {
-        await removeAllRules();
+        stopExtension();
     }
 })
 
-async function removeAllRules() {
-    const oldRules = await chrome.declarativeNetRequest.getDynamicRules();
-    chrome.declarativeNetRequest.updateDynamicRules(
-        {removeRuleIds: oldRules.map(e => e.id)},
-        () => console.log("block rule added")
-    );
+function startExtension() {
+
+}
+
+function stopExtension() {
+
 }
